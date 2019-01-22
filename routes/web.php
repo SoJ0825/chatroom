fc3c8084cd331680ed05951c6b4a11332af58974
SOJ <takayashi0825@gmail.com>
2019-01-22 17:45:29 +0800

    pusher OK

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Task;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks', function () {
+    return Task::latest()->pluck('body');
+});
+
+Route::post('/tasks', function () {
+    $task = Task::forceCreate(request(['body']));
+
+    event(
+        (new \App\Events\TaskCreated($task))->dontBroadcastToCurrentUser());
+});
