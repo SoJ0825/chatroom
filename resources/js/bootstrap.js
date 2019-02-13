9b9c38b5d118917588628362838c086f2b7b476a
SOJ <takayashi0825@gmail.com>
2019-02-13 21:07:06 +0800

    use ngrok for socket uri

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -60,5 +60,6 @@ import Echo from 'laravel-echo'
 window.io = require('socket.io-client');
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001'
+    // host: window.location.hostname + ':6001'
+    host: 'http://ac78b079.ngrok.io'
 });
