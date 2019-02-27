206c69f982b07236c2279435b4a2233bc0594c6f
SOJ <takayashi0825@gmail.com>
2019-02-27 11:32:02 +0800

    do not use laravel-echo in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -58,8 +58,8 @@ if (token) {
 import Echo from 'laravel-echo'
 
 window.io = require('socket.io-client');
-window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    // host: window.location.hostname + ':6001'
-    host: 'http://45fecea5.ngrok.io'
-});
+// window.Echo = new Echo({
+//     broadcaster: 'socket.io',
+//     host: window.location.hostname + ':6001'
+//     // host: 'http://45fecea5.ngrok.io'
+// });
